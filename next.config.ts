74492d7a146e922666c7c52787e0e0daa872cc77
bymacy <macytemblique16@gmail.com>
2026-08-06 19:35:55 +0800

    Add project page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import createMDX from '@next/mdx'
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+}
 
-export default nextConfig;
+export default createMDX({})(nextConfig)
